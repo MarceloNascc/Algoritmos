c35c61c8475d43e869ea769516f81dbd62599dbd
João Marcelo <31549089+MarceloNascc@users.noreply.github.com>
2019-02-28 15:53:31 -0300

    Add files via upload

---
+++ SubArrayMax.py
@@ -0,0 +1,58 @@
+#VERIFICA INTERSEÇÃO
+def SubArrayMaxInt(lista, ini, meio, fim):
+  somaE = float('-inf')
+  indiceE = -1
+  soma = 0
+  #VERIFICA A MAIOR SOMA DA ESQUERDA A PARTIR DO MEIO
+  for i in range(meio, -1, -1):
+    soma += lista[i]
+    if soma > somaE:
+      indiceE = i
+      somaE = soma
+
+  somaD = float('-inf')
+  indiceD = -1
+  soma = 0
+  #VERIFICA A MAIOR SOMA DA DIREITA A PARTIR DO MEIO
+  for i in range(meio+1, fim+1):
+    soma += lista[i]
+    if soma > somaD:
+      indiceD = i
+      somaD = soma
+
+  return (indiceE, indiceD, somaE + somaD)
+
+
+def SubArrayMax(lista, ini, fim):
+  if ini == fim:
+    return (ini, fim, lista[ini])
+
+  meio = (ini+fim)//2
+  #print(meio)
+
+  maxE = SubArrayMax(lista, ini, meio)
+  maxD = SubArrayMax(lista, meio+1, fim)
+  maxI = SubArrayMaxInt(lista, ini, meio, fim)
+  #print(maxI)
+
+  if maxE[2] > maxD[2] and maxE[2] > maxI[2]:
+    return maxE
+  elif maxD[2] > maxE[2] and maxD[2] > maxI[2]:
+    return maxE
+  else:
+    return maxI
+
+#TESTE
+def main():
+  lista = []
+  n = int(input("Tamanho: "))
+
+  for i in range(n):
+    lista.append(int(input()))
+
+  res = SubArrayMax(lista, 0, len(lista) - 1)
+
+  print(res)
+
+if __name__ == "__main__":
+  main()
